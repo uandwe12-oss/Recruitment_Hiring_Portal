bf854e913be7dc7f8eeb2d86010dc21db987950a
uandwe12-oss <uandwe12@gmail.com>
2026-02-18 20:42:45 +0530

    Update candidates.js

--- HR_Backend/api/candidates.js
+++ HR_Backend/api/candidates.js
@@ -10,7 +10,7 @@ const getDriver = require("../lib/neo4j");
 
 // Add CORS headers to all routes in this router
 router.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'https://recruitment-hiring-portal-ibsf.vercel.app');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Credentials', 'true');
