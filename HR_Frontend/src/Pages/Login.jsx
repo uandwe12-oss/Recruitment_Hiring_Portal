dce3af724437149bf2558b89164e8c70ead8ef53
uandwe12-oss <uandwe12@gmail.com>
2026-02-18 20:24:17 +0530

    Update Login.jsx

--- HR_Frontend/src/Pages/Login.jsx
+++ HR_Frontend/src/Pages/Login.jsx
@@ -47,7 +47,7 @@ const Login = () => {
     setIsSubmitting(true);
 
     try {
-      const res = await fetch("http://localhost:5000/api/login", {
+      const res = await fetch("https://recruitment-hiring-portal.vercel.app/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username: email, password }),
