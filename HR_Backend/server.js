9629b10aabf1aed7aa19ba13c810def90b04e8e4
Dharanish V <vdharan@kyotralis.com>
2026-03-04 19:50:39 +0530

    finally all done again1

--- HR_Backend/server.js
+++ HR_Backend/server.js
@@ -8,7 +8,10 @@ const path = require("path");
 const app = express();
 
 const corsOptions = {
-  origin: "https://recruitment-hiring-portal-ibsf.vercel.app",
+  origin: [
+  "https://recruitment-hiring-portal.vercel.app",
+  "https://recruitment-hiring-portal-ibsf.vercel.app"
+],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
