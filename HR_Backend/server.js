4ec87ef51099ac7522bede305c67978dbf919391
uandwe12-oss <uandwe12@gmail.com>
2026-04-01 23:58:37 +0530

    Update server.js

--- HR_Backend/server.js
+++ HR_Backend/server.js
@@ -104,8 +104,3 @@ app.use((err, req, res, next) => {
 module.exports = app;
 
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
